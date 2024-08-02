33a21938e191ddee4258eef69269d0e281de4acd
Amire Steve <zoe_steve@hotmail.com>
2024-08-02 15:55:16 +0100

    APL-849: Refactor OptionModal.vue class declarations
    Consolidate multiline class declarations into single lines for improved readability. This change reduces redundancy and makes the template code more concise without affecting functionality.

--- components/sections/OptionModal.vue
+++ components/sections/OptionModal.vue
@@ -1,21 +1,7 @@
 <template>
   <transition appear name="fade">
     <div
-      class="
-        modal
-        fixed
-        bg-[rgba(0,0,0,.7)]
-        text-white
-        top-0
-        bottom-0
-        right-0
-        left-0
-        flex
-        justify-center
-        items-center
-        px-4
-        md:px-0
-      "
+      class="modal fixed bg-[rgba(0,0,0,.7)] text-white top-0 bottom-0 right-0 left-0 flex justify-center items-center px-4 md:px-0"
     >
       <div class="modal-box bg-white py-8 md:py-20 px-6 md:px-10">
         <div class="-mt-6 md:-mt-16 md:-mr-4 -mr-4 flex justify-end">
@@ -24,15 +10,7 @@
           </button>
         </div>
         <h2
-          class="
-            uppercase
-            text-primary
-            font-inter
-            text-xl
-            md:text-2xl
-            font-bold
-            mb-6
-          "
+          class="uppercase text-primary font-inter text-xl md:text-2xl font-bold mb-6"
         >
           Select preferred format
         </h2>
@@ -64,11 +42,7 @@ export default {
   methods: {
     selectFormat(itemSelected) {
       this.types.forEach((type) => {
-        if (type === itemSelected) {
-          type.selected = true
-        } else {
-          type.selected = false
-        }
+        type.selected = type === itemSelected
       })
       this.$emit('selectItem', itemSelected)
     },
